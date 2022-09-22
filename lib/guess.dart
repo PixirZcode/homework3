5f14cf3a2ff031440c78247ea88b458674acba94
Krisanapong Ongkamharn <ongkamharn_k@silpakorn.edu>
2022-09-22 20:21:54 +0700

    Initial commit

---
+++ lib/guess.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+class Game {
+  int answer = 0; // instance field
+  int count = 0; // นับรอบที่ทาย
+
+  int get totalGuesses{
+    return this.count;
+  }
+  Game({int maxRandom = 100}) {
+    var r = Random();
+    answer = r.nextInt(maxRandom) + 1;
+    print('│◆ คำตอบคือ $answer');
+  }
+
+  int doGuess(int num) {
+    if (num > answer) {
+      count += 1;
+      print('│➧ $num is too high ▲');
+      return 0;
+    } else if (num < answer) {
+      count += 1;
+      print('│➧ $num is too low ▼');
+      return -1;
+    } else {
+      count += 1;
+      print('│➧ $num is correct ✔, total guesses: $count');
+      return 1;
+    }
+  }
+}
